1704da69e0706ef71c067c543b540ec3e2e6c2d0
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-30 21:55:45 +0000

    add type of form

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <html>
   <head>
     <title>PHP Test</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
   </head>
   <body>
+    <main class="container">
+      <div class="row">
+        <div class="col">
+          
   <h1>Installation et utilisation des formlaires</h1>
     <p>
       Les formulaires offrent une véritable interaction avec l'utilisateur.non seulement elles apportent des informations précieuses mais permettent aussio d'évaluer et de mesurer son intéret pour le contenu du site Web. <br>
@@ -10,9 +16,35 @@
     </p>
     <h2>Les trois étapes d'un formulaire</h2>
      <ul>
-       <li>>Construire un formulaire dans un controlleur ou en utilisant une classe de formulaire dans le dossier Form</li>
+       <li>Construire un formulaire dans un controlleur ou en utilisant une classe de formulaire dans le dossier Form</li>
        <li>Rendre le formulaire dans un modèle, c-à-dire faire en sorte que le controlleur appelle une page twig avec les champs du formulaire</li>
        <li>Traiter le formulaire ,ce qui implique une validation est une trnasformation des données, le plus souvent en projet pour pouvoir etre transférerr dans une absse de données.</li>
      </ul>
+    <h2>Installation dans un projet Symfony</h2>
+    <p>
+      dans les applications Symfony Flex , qui est facultatifmeme s'il est installer par défaut depuis Symfony 4, vous pouvez installer Smfony Form avec la ligne de commande  suivante: <br><br>
+
+      * $ composer require Symfony/form
+    </p>
+    <br><br>
+
+    <h2>Type de formulaire et type de champs</h2>
+          <p>
+            Un "type" de formulaire est une classe PHP.
+          </p>
+
+        </div>
+      </div>
+    </main>
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
   </body>
 </html>
