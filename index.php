4ac3f737cb7554af272cc6581cba8873e5074d9a
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-30 21:34:42 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+  <h1>Installation et utilisation des formlaires</h1>
+    <p>
+      Les formulaires offrent une véritable interaction avec l'utilisateur.non seulement elles apportent des informations précieuses mais permettent aussio d'évaluer et de mesurer son intéret pour le contenu du site Web. <br>
+      Pour l'internaute ,remplir un formulaire nécessite de la confiance. Cela engage le site à lettre en place une sécurisation des données et parfois de les crypter ou les hasher comme les mots de passe par exemple.
+    </p>
+  </body>
+</html>
