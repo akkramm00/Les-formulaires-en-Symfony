3952ea23e63aa73546e12c6f20f54a4ff5626716
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-30 21:45:10 +0000

    add steps for create a form whith symfony

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
       Les formulaires offrent une véritable interaction avec l'utilisateur.non seulement elles apportent des informations précieuses mais permettent aussio d'évaluer et de mesurer son intéret pour le contenu du site Web. <br>
       Pour l'internaute ,remplir un formulaire nécessite de la confiance. Cela engage le site à lettre en place une sécurisation des données et parfois de les crypter ou les hasher comme les mots de passe par exemple.
     </p>
+    <h2>Les trois étapes d'un formulaire</h2>
+     <ul>
+       <li>>Construire un formulaire dans un controlleur ou en utilisant une classe de formulaire dans le dossier Form</li>
+       <li>Rendre le formulaire dans un modèle, c-à-dire faire en sorte que le controlleur appelle une page twig avec les champs du formulaire</li>
+       <li>Traiter le formulaire ,ce qui implique une validation est une trnasformation des données, le plus souvent en projet pour pouvoir etre transférerr dans une absse de données.</li>
+     </ul>
   </body>
 </html>
